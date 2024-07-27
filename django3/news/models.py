80a62779ced8255dad24c85a06a25b8e981ff654
Kvitok8320 <153814702+Kvitok8320@users.noreply.github.com>
2024-07-27 11:55:44 +0500

    add atr in 1 model

--- django3/news/models.py
+++ django3/news/models.py
@@ -5,3 +5,4 @@ class news_posts(models.Model):
     title = models.CharField('Название')
     short_descr = models.CharField('Кратко', max_length=200)
     content = models.TextField('Содержание')
+    pub_date = models.DateTimeField('Дата')
