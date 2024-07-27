558b3ca87355015652583f579ea69b7395fd8be0
Kvitok8320 <153814702+Kvitok8320@users.noreply.github.com>
2024-07-27 11:53:54 +0500

    created 1 model

--- django3/news/models.py
+++ django3/news/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class news_posts(models.Model):
+    title = models.CharField('Название')
+    short_descr = models.CharField('Кратко', max_length=200)
+    content = models.TextField('Содержание')
