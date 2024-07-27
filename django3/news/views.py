7aae194e7f7e7f387507ed5ddc4c3d2f4cd2bda5
Kvitok8320 <153814702+Kvitok8320@users.noreply.github.com>
2024-07-27 12:55:04 +0500

    models works

--- django3/news/views.py
+++ django3/news/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import news_posts
 
 def home(request):
     # return HttpResponse("<h1>Это мой первый проект на Django</h1>")
-    return render(request, 'news/news_page.html')
+    news = news_posts.objects.all()
+    return render(request, 'news/news_page.html',{'news': news})
