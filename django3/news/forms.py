dfed7ec5850b0bac9c4a49ec5725653ae411fa28
Kvitok8320 <153814702+Kvitok8320@users.noreply.github.com>
2024-07-27 15:25:36 +0500

    replace djangoform

--- django3/news/forms.py
+++ django3/news/forms.py
@@ -1,7 +1,13 @@
 from .models import news_posts
-from django.forms import ModelForm
+from django.forms import ModelForm, TextInput, Textarea, DateTimeInput
 
 class news_postsForm(ModelForm):
 	class Meta:
 		model = news_posts
 		fields = ['title', 'short_descr', 'content', 'pub_date']
+		widgets = {
+			'title': TextInput(attrs={'class': 'form-control', 'placeholder': 'Заголовок новости'}),
+			'short_descr': TextInput(attrs={'class': 'form-control', 'placeholder': 'Краткое описание новости'}),
+			'content': Textarea(attrs={'class': 'form-control', 'placeholder': 'Содержание новости'}),
+			'pub_date': DateTimeInput(attrs={'class': 'form-control', 'placeholder': 'Дата публикации'})
+		}
