105e681658d51b30af8d8b04483821f2d7005cfd
Kvitok8320 <153814702+Kvitok8320@users.noreply.github.com>
2024-07-27 12:44:31 +0500

    work with models

--- django3/news/models.py
+++ django3/news/models.py
@@ -2,7 +2,11 @@ from django.db import models
 
 # Create your models here.
 class news_posts(models.Model):
-    title = models.CharField('Название')
+    title = models.CharField('Название', max_length=100)
     short_descr = models.CharField('Кратко', max_length=200)
     content = models.TextField('Содержание')
     pub_date = models.DateTimeField('Дата')
+
+    class Meta:
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
--- django3/news/views.py
+++ django3/news/views.py
@@ -1,3 +1,6 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    # return HttpResponse("<h1>Это мой первый проект на Django</h1>")
+    return render(request, 'news/news_page.html')
