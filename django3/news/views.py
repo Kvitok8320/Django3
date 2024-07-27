78efee9cc0cd318e11c96bed5fc2e446661f4ff8
Kvitok8320 <153814702+Kvitok8320@users.noreply.github.com>
2024-07-27 15:41:55 +0500

    complete homework 4

--- django3/news/views.py
+++ django3/news/views.py
@@ -1,6 +1,4 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-from .models import news_posts
+from django.shortcuts import render, redirect
 from .models import news_posts
 from .forms import news_postsForm
 
@@ -10,6 +8,14 @@ def home(request):
     return render(request, 'news/news_page.html',{'news': news})
 
 def add_news(request):
+    error = ""
+    if request.method == 'POST':
+        form = news_postsForm(request.POST)  # Сюда сохранится информация от пользователя.
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = "Данные были заполнены некорректно"
     form = news_postsForm()
     # return HttpResponse("<h1>Это мой первый проект на Django</h1>")
-    return render(request, 'news/create_news.html', {'form': form})
+    return render(request, 'news/create_news.html', {'form': form, 'error': error})
